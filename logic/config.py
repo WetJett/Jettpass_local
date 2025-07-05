3fc9802238b691c91ef7150f11e37440083d2f3d
WetJett <lizardbrovary@gmail.com>
2025-07-05 12:17:59 +0200

    mod on  linux

--- logic/config.py
+++ logic/config.py
@@ -1,3 +1,5 @@
+import os
+
 #for users.jett file
 LOGIN_SIZE = 128
 PASSWORD_SIZE = 64
@@ -5,12 +7,22 @@ SALT_SIZE = 16
 DATE_SIZE = 8
 HEADER_SIZE = 4  # accounts count (uint32)
 RECORD_SIZE = LOGIN_SIZE + PASSWORD_SIZE + SALT_SIZE + DATE_SIZE  # = 216
-USER_LIST = "E:/!dump/JettpassGUI/pswds/users.jett"
+#USER_LIST = "E:/!dump/JettpassGUI/pswds/users.jett"
+if os.name == "posix":
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    pswds_folder = os.path.join(current_directory, "pswds")
+
+    if not os.path.exists(pswds_folder):
+        os.makedirs(pswds_folder)
+    USER_LIST = os.path.join(pswds_folder, "users.jett")
+    DUMP_DIRECTORY = pswds_folder
+else:
+    USER_LIST = "E:/!dump/JettpassGUI/pswds/users.jett" #for me 
+    DUMP_DIRECTORY = "E:/!dump/JettpassGUI/pswds"
 
 #for [username].dump file
 MAGIC_NUMBER = b'JDF1'
 DUMP_SALT = 16  
-DUMP_DIRECTORY = "E:/!dump/JettpassGUI/pswds"
 HEADER_SIZE_DUMP = 24 # MAGIC + SALT + COUNTER
 ALIAS_SIZE = 64 
 IV_SIZE = 12
